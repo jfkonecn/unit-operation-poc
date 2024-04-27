f854d7fa86d95616ecc74ff4e8444381f2b37ae4
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-04-27 12:40:58 -0400

    added stub map function

--- svg-builder/src/build-svgs.ts
+++ svg-builder/src/build-svgs.ts
@@ -1,40 +1,11 @@
-import { select } from "d3";
-import { JSDOM } from "jsdom";
-import * as fs from "fs";
-import * as path from "node:path";
-import * as consts from "./consts.ts";
+import { addMap, createSvgBuilder } from "./utils.ts";
 
-interface CreateSvgBuilderArgs {
-  height?: number;
-  width?: number;
-}
-
-function createSvgBuilder({
-  height = 100,
-  width = 100,
-}: CreateSvgBuilderArgs = {}) {
-  const dom = new JSDOM(`<body></body>`);
-
-  const body = select(dom.window.document.querySelector("body"));
-  const svg = body
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("xmlns", "http://www.w3.org/2000/svg");
-
-  return {
-    svg,
-    saveToFile: (fileName: string) => {
-      fs.writeFileSync(path.join(consts.pathToSvg, fileName), body.html());
-    },
-  };
-}
 const { svg, saveToFile } = createSvgBuilder();
-svg
-  .append("rect")
-  .attr("x", 10)
-  .attr("y", 10)
-  .attr("width", 80)
-  .attr("height", 80)
-  .style("fill", "green");
+addMap({
+  height: 100,
+  width: 100,
+  x: 10,
+  y: 10,
+  svg,
+});
 saveToFile("out.svg");
---
+++ svg-builder/src/utils.ts
@@ -0,0 +1,53 @@
+import { select } from "d3";
+import { JSDOM } from "jsdom";
+import * as fs from "fs";
+import * as path from "node:path";
+import * as consts from "./consts.ts";
+import { type Selection } from "d3-selection";
+
+interface CreateSvgBuilderArgs {
+  height?: number;
+  width?: number;
+}
+
+export function createSvgBuilder({
+  height = 100,
+  width = 100,
+}: CreateSvgBuilderArgs = {}): {
+  svg: Selection<SVGSVGElement, unknown, null, undefined>;
+  saveToFile: (fileName: string) => void;
+} {
+  const dom = new JSDOM(`<body></body>`);
+
+  const body = select(dom.window.document.querySelector("body"));
+  const svg = body
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("xmlns", "http://www.w3.org/2000/svg");
+
+  return {
+    svg,
+    saveToFile: (fileName: string) => {
+      fs.writeFileSync(path.join(consts.pathToSvg, fileName), body.html());
+    },
+  };
+}
+
+interface AddSvgItemArgs {
+  height: number;
+  width: number;
+  x: number;
+  y: number;
+  svg: Selection<SVGSVGElement, unknown, null, undefined>;
+}
+
+export function addMap({ height, width, x, y, svg }: AddSvgItemArgs) {
+  svg
+    .append("rect")
+    .attr("x", x)
+    .attr("y", y)
+    .attr("width", width)
+    .attr("height", height)
+    .style("fill", "green");
+}
