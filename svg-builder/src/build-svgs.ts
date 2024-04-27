09a6338bb355ffbf009cc76c8128cdaadf2d7f58
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-04-27 12:27:38 -0400

    made svg function

--- svg-builder/src/build-svgs.ts
+++ svg-builder/src/build-svgs.ts
@@ -4,19 +4,37 @@ import * as fs from "fs";
 import * as path from "node:path";
 import * as consts from "./consts.ts";
 
-const dom = new JSDOM(`<body></body>`);
+interface CreateSvgBuilderArgs {
+  height?: number;
+  width?: number;
+}
 
-const body = select(dom.window.document.querySelector("body"));
-const svg = body
-  .append("svg")
-  .attr("width", 100)
-  .attr("height", 100)
-  .attr("xmlns", "http://www.w3.org/2000/svg");
+function createSvgBuilder({
+  height = 100,
+  width = 100,
+}: CreateSvgBuilderArgs = {}) {
+  const dom = new JSDOM(`<body></body>`);
+
+  const body = select(dom.window.document.querySelector("body"));
+  const svg = body
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("xmlns", "http://www.w3.org/2000/svg");
+
+  return {
+    svg,
+    saveToFile: (fileName: string) => {
+      fs.writeFileSync(path.join(consts.pathToSvg, fileName), body.html());
+    },
+  };
+}
+const { svg, saveToFile } = createSvgBuilder();
 svg
   .append("rect")
   .attr("x", 10)
   .attr("y", 10)
   .attr("width", 80)
   .attr("height", 80)
-  .style("fill", "red");
-fs.writeFileSync(path.join(consts.pathToSvg, "out.svg"), body.html());
+  .style("fill", "green");
+saveToFile("out.svg");
