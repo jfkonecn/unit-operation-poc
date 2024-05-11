607af010689dec2afa19330941cae9be3e7bda60
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-05-11 09:01:15 -0400

    defined operation types

--- svg-builder/src/unit-operator-graph-builders.ts
+++ svg-builder/src/unit-operator-graph-builders.ts
@@ -1,46 +1,98 @@
-type RootType = "global_state_read" | "io";
-type CustomArray = [foo: "foo", ...middle: "bar"[], baz: "baz"];
+//accepts: "simple" | "unit";
+//returns: "simple" | "result" | "unit";
+import { type Selection } from "d3-selection";
 
-export type UnitOperation = {
-  label: string;
-} & (
-  | {
-      type:
-        | "map"
-        | "filter"
-        | "sort"
-        | "global_state_read"
-        | "io"
-        | "distribution"
-        | "panic";
-      nextIndex: number;
-    }
-  | {
-      type:
-        | "map"
-        | "filter"
-        | "sort"
-        | "validate"
-        | "authenticate"
-        | "authorize"
-        | "global_state_read"
-        | "global_state_write"
-        | "io"
-        | "distribution"
-        | "panic";
-    }
-  | {
-      type:
-        | "map"
-        | "filter"
-        | "sort"
-        | "validate"
-        | "authenticate"
-        | "authorize"
-        | "global_state_read"
-        | "global_state_write"
-        | "io"
-        | "distribution"
-        | "panic";
-    }
-);
+type UnitOutput = { unitOutput: true };
+
+type SimpleOutput = {
+  successIndex: number;
+  errorIndex: number;
+};
+
+type ResultOutput = {
+  successIndex: number;
+  errorIndex: number;
+};
+
+type GlobalStateReadOperation = {
+  type: "global_state_read";
+} & SimpleOutput;
+
+type RootIoOperation = {
+  type: "io";
+} & SimpleOutput;
+
+type MiddleIoOperation = {
+  type: "io";
+} & (SimpleOutput | ResultOutput | UnitOutput);
+
+type EndIoOperation = {
+  type: "io";
+} & UnitOutput;
+
+type IoOperation = RootIoOperation | MiddleIoOperation | EndIoOperation;
+
+type MapOperation = {
+  type: "map";
+} & SimpleOutput;
+
+type FilterOperation = {
+  type: "filter";
+} & SimpleOutput;
+
+type SortOperation = {
+  type: "sort";
+} & SimpleOutput;
+
+type ValidateOperation = {
+  type: "validate";
+} & ResultOutput;
+
+type AuthenticateOperation = {
+  type: "authenticate";
+} & ResultOutput;
+
+type AuthorizeOperation = {
+  type: "authorize";
+} & ResultOutput;
+
+type GlobalStateWriteOperation = {
+  type: "global_state_write";
+} & UnitOutput;
+
+type DistributionOperation = {
+  type: "distribution";
+} & SimpleOutput;
+
+type PanicOperation = {
+  type: "panic";
+} & UnitOutput;
+
+type StartOperation = RootIoOperation | GlobalStateReadOperation;
+type EndOperation = EndIoOperation | PanicOperation | GlobalStateWriteOperation;
+
+type MiddleOperation =
+  | StartOperation
+  | EndOperation
+  | MapOperation
+  | FilterOperation
+  | IoOperation
+  | SortOperation
+  | ValidateOperation
+  | AuthenticateOperation
+  | AuthorizeOperation
+  | DistributionOperation;
+type OperationFlow = [
+  start: StartOperation,
+  ...middle: MiddleOperation[],
+  end: EndOperation,
+];
+
+export function drawOperationFlow(
+  svg: Selection<SVGSVGElement, unknown, null, undefined>,
+  flow: OperationFlow,
+) {
+  console.log(svg);
+  console.log(flow);
+  return;
+}
