35adf8405d8700b1a51ee47b4970dbfd02ed950f
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-08-10 13:50:17 -0400

    Added CSharp

---
+++ csharp/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+dotnet run --project $SCRIPT_DIR/UnitOperations.Console $1
---
+++ run-performance-tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+LANGUAGE="csharp"
+TEST_FILE_NAME="$(echo $ROW_COUNT)_rows.csv"
+ROW_COUNT=1
+
+
+LANGUAGES=("csharp")
+
+for LANGUAGE in "${LANGUAGES[@]}"; do
+    echo "Processing language: $LANGUAGE"
+    for FILE in $SCRIPT_DIR/data-generation/test-data/*_rows.csv; do
+        FILENAME=$(basename "$FILE")
+        TOTAL_RECORDS=${FILENAME%%_*}
+
+        echo "Processing $FILENAME"
+
+        LANGUAGE_DIR="$SCRIPT_DIR/$LANGUAGE"
+        RUN_SCRIPT="$LANGUAGE_DIR/run.sh"
+        eval "$RUN_SCRIPT $FILE $TOTAL_RECORDS"
+    done
+    echo "Finished processing language: $LANGUAGE"
+    echo "----------------------------------------"
+done
