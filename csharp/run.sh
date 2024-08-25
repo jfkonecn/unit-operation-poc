7a4442c10cdc180d304fe57a0d767ff956e3d7a0
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-08-25 11:36:03 -0400

    run dotnet in release mode

--- csharp/run.sh
+++ csharp/run.sh
@@ -3,4 +3,4 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-dotnet run --project $SCRIPT_DIR/UnitOperations.Console $1 $2 $3
+dotnet run --configuration Release --project $SCRIPT_DIR/UnitOperations.Console $1 $2 $3
