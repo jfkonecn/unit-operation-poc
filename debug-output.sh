8a258056629d7d9e7992e972f8cfc137c6ef19b8
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-08-25 13:55:56 -0400

    added debug script

---
+++ debug-output.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <language> <total_records>"
+    exit 1
+fi
+
+LANGUAGE=$1
+TOTAL_RECORDS=$2
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+CYCLES="$SCRIPT_DIR/performance-utils/cycles"
+FILE="$SCRIPT_DIR/data-generation/test-data/"$TOTAL_RECORDS"_rows.csv"
+
+LANGUAGE_DIR="$SCRIPT_DIR/$LANGUAGE"
+RUN_SCRIPT="$LANGUAGE_DIR/run.sh"
+
+eval "$RUN_SCRIPT $FILE $TOTAL_RECORDS $CYCLES"
