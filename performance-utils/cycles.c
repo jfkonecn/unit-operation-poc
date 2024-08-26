95f306e61573db233b0aba45d8b41a4fa05f4fe1
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-08-25 20:12:39 -0400

    arm cycle count

--- performance-utils/cycles.c
+++ performance-utils/cycles.c
@@ -1,6 +1,33 @@
 #include <stdint.h>
 #include <stdio.h>
-#include <x86intrin.h>
+
+uint64_t readCpuTimer() { 
+#if defined(__i386__)
+  int64_t ret;
+  __asm__ volatile("rdtsc" : "=A"(ret));
+  return ret;
+#elif defined(__x86_64__) || defined(__amd64__)
+  uint64_t low, high;
+  __asm__ volatile("rdtsc" : "=a"(low), "=d"(high));
+  return (high << 32) | low;
+#elif defined(__ARM_ARCH)
+    uint64_t val;
+    // https://stackoverflow.com/questions/40454157/is-there-an-equivalent-instruction-to-rdtsc-in-arm
+
+    /*
+     * According to ARM DDI 0487F.c, from Armv8.0 to Armv8.5 inclusive, the
+     * system counter is at least 56 bits wide; from Armv8.6, the counter
+     * must be 64 bits wide.  So the system counter could be less than 64
+     * bits wide and it is attributed with the flag 'cap_user_time_short'
+     * is true.
+     */
+    asm volatile("mrs %0, cntvct_el0" : "=r" (val));
+
+    return val;
+#else
+#error You need to define CycleTimer for your OS and CPU
+#endif
+}
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -9,7 +36,7 @@ int main(int argc, char *argv[]) {
   }
 
   char *name = argv[1];
-  uint64_t cycles = __rdtsc();
+  uint64_t cycles = readCpuTimer();
 
   printf("%s,%lu\n", name, cycles);
 
