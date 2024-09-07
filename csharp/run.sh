795a645a5aa7eec45b17b324b7e42d4005030ee1
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-09-07 10:32:57 -0400

    Updates

--- csharp/run.sh
+++ csharp/run.sh
@@ -3,4 +3,4 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-dotnet run --no-build --configuration Release --project $SCRIPT_DIR/UnitOperations.Console
+dotnet run --no-build --configuration Release --project $SCRIPT_DIR/UnitOperations.Console $1 $2 $3
