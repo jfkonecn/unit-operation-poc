77e35d1736ee9a275d83b0e3ffd52de8b349efd2
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-09-21 15:52:57 -0400

    removed nvm call from javascript

--- languages/javascript/build.sh
+++ languages/javascript/build.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-
-cd "$SCRIPT_DIR"
-
-nvm install
--- languages/javascript/run.sh
+++ languages/javascript/run.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd $SCRIPT_DIR
-
-nvm use > /dev/null
-
 
 npm start --silent $1 $2 $3
